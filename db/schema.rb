e07ddde4d72286f3af536548778e6e06eb7e8d62
bennygrub <blgruber@gmail.com>
2017-06-30 09:47:52 -0700

    adding category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629044416) do
+ActiveRecord::Schema.define(version: 20170630163744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -36,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170629044416) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "images"
+    t.integer "category_id"
     t.index ["slug"], name: "index_products_on_slug", unique: true
   end
 
