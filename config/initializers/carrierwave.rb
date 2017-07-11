2a4bb7cffdbd33756ddc920e3efbc6f7e1fec8b0
bennygrub <blgruber@gmail.com>
2017-07-11 16:19:31 -0700

    us-west-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,6 +14,7 @@ unless Rails.env.development?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET'],
+      region:                'us-west-2' 
     }
 
     # For testing, upload files to local `tmp` folder.
